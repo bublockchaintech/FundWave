a7aa233f1826028bb71ae0bc6e25c4f920e11a74
erenyusufduran <eren.duran@colins.com>
2023-05-21 16:50:00 +0300

    adding head for fonts and fontawesome

--- client/src/pages/projects/index.js
+++ client/src/pages/projects/index.js
@@ -1,9 +1,27 @@
 import styles from "@/styles/Projects.module.css";
+import Head from "next/head";
 
 export default function () {
   return (
-    <div className="container">
-      <div className={styles.name}>Eren</div>
-    </div>
+    <>
+      <Head>
+        <title>Font Wave | Projects</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap" rel="stylesheet" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+          integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+      </Head>
+      <div className={styles.s}>
+        <div className="container">
+          <div className={styles.name}>Eren</div>
+        </div>
+      </div>
+    </>
   );
 }
