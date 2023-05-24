fc5e52a9824bc0c75bb4d93fcca9a5adfd388fc5
erenyusufduran <eren.duran@colins.com>
2023-05-24 15:41:26 +0300

    layout

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,20 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@/styles/globals.css";
-import { Navbar } from "@/components/Navbar";
-import { Footer } from "@/components/Footer";
-import { useEffect, useState } from "react";
+import { Layout } from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
-  const [isBrowser, setIsBrowser] = useState(false);
-
-  useEffect(() => {
-    setIsBrowser(typeof window !== "undefined");
-  }, []);
   return (
-    <>
-      <Navbar />
-      {isBrowser && <Component {...pageProps} />}
-      <Footer />
-    </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
