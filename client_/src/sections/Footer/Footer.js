57347b7b0f1b435e0751c802200cf318ac48432c
erenyusufduran <eren.duran@colins.com>
2023-05-25 21:14:09 +0300

    footer

---
+++ client_/src/sections/Footer/Footer.js
@@ -0,0 +1,27 @@
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="social">
+        <a href="https://www.instagram.com/bublockchaintechclub/">
+          <i className="fab fa-instagram fa-2x" />
+        </a>
+        <a href="https://twitter.com/BU_blockchain">
+          <i className="fab fa-twitter fa-2x" />
+        </a>
+        <a href="">
+          <i className="fab fa-youtube fa-2x" />
+        </a>
+        <a href="https://www.linkedin.com/company/blockchaintechclub/mycompany/">
+          <i className="fab fa-linkedin fa-2x" />
+        </a>
+      </div>
+      <div className="container text-center">
+        <span>All Rights Reserved. @2025</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
