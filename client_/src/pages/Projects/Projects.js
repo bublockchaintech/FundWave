2e8a7e8fab71d34e11c7f879df9ea3cab3266bbd
erenyusufduran <eren.duran@colins.com>
2023-05-26 00:10:00 +0300

    fund project

--- client_/src/pages/Projects/Projects.js
+++ client_/src/pages/Projects/Projects.js
@@ -1,14 +1,57 @@
 import React, { useState } from "react";
 import { CreateProject, ExecuteProject, FundProject } from "../../sections";
 
+const projects = [
+  {
+    project_name: "PROJECT NAME",
+    community_address: "0x343...2321",
+    totalFunds: 342.123,
+    totalVotes: 1.123,
+    subject: "Subject",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio repudiandae reprehenderit praesentium possimus. Quis consequatur pariatur fuga blanditiis voluptatibus eaque?",
+  },
+  {
+    project_name: "PROJECT NAME",
+    community_address: "0x343...2321",
+    totalFunds: 342.123,
+    totalVotes: 1.123,
+    subject: "Subject",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio repudiandae reprehenderit praesentium possimus. Quis consequatur pariatur fuga blanditiis voluptatibus eaque?",
+  },
+  {
+    project_name: "PROJECT NAME",
+    community_address: "0x343...2321",
+    totalFunds: 342.123,
+    totalVotes: 1.123,
+    subject: "Subject",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio repudiandae reprehenderit praesentium possimus. Quis consequatur pariatur fuga blanditiis voluptatibus eaque?",
+  },
+  {
+    project_name: "PROJECT NAME",
+    community_address: "0x343...2321",
+    totalFunds: 342.123,
+    totalVotes: 1.123,
+    subject: "Subject",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio repudiandae reprehenderit praesentium possimus. Quis consequatur pariatur fuga blanditiis voluptatibus eaque?",
+  },
+  {
+    project_name: "PROJECT NAME",
+    community_address: "0x343...2321",
+    totalFunds: 342.123,
+    totalVotes: 1.123,
+    subject: "Subject",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio repudiandae reprehenderit praesentium possimus. Quis consequatur pariatur fuga blanditiis voluptatibus eaque?",
+  },
+];
+
 const Projects = () => {
-  const [stageStage, setStageStage] = useState("CREATE_PROJECT");
+  const [stageStage, setStageStage] = useState("FUND_PROJECT");
 
   return (
     <>
       {/* Project Cards */}
       {stageStage === "CREATE_PROJECT" && <CreateProject />}
-      {stageStage === "FUND_PROJECT" && <FundProject />}
+      {stageStage === "FUND_PROJECT" && <FundProject projects={projects} />}
       {stageStage === "EXECUTE_PROJECT" && <ExecuteProject />}
     </>
   );
